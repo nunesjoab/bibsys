4aacf65457d6796b82e659fa011db3ecab596738
nunesjoab <nunesjoab@gmail.com>
2017-05-30 01:15:59 -0300

    html PURO
    index.html puro funcionando

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * waioDev sk3LTn
+ * Configurações
+ *
+ */
+
+
+// SITE META
+define('SITE_URI', 'http://');
+define('SITE_LANGUAGE', 'pt_BR');
+define('SITE_TITLE', 'waioDev Simple Framework');
+define('SITE_DESCRIPTION', 'Framework de desenvolvimento para projetos simples');
+
+
+// FACEBOOK
+define('FB_OGG', true);
+define('FB_URI', 'http://facebook.com/');
+define('FB_NAME', 'website');
+
+
+// FUNCTIONS
+require_once('includes/functions.php');
---
+++ footer.php
@@ -0,0 +1,30 @@
+<footer class="main-footer">
+
+	<div class="inner-footer">
+		<div class="container">
+			
+			<div class="copyright" itemscope itemtype="http://schema.org/CreativeWork">
+				© <span itemprop="copyrightYear">2017</span> - <span itemprop="copyrightHolder">BibSys</span> - Todos os direitos reservados
+				<a itemprop="author" href="#" title="Desenvolvido por Guilherme, Joab e Webster" rel="author" >Desenvolvido por Guilherme, Joab e Webster</a>
+			</div><!--.copyright-->
+			
+		</div><!-- .container -->
+
+
+	</div><!-- .inner-footer -->
+</footer> <!--footer -->
+
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<script src="assets/js/scripts.js"></script>
+
+<script>
+WebFont.load({
+ google: {
+  families: ['Open Sans:400,400i,700,800']
+ }
+});
+</script>
+</body>
+</html>
