93e0b546d9fd24acb5a2454c9c0cbef389a47bcd
Webster Lima <websterhlima@gmail.com>
2017-06-22 00:52:12 -0300

    serviços

--- src/main/java/br/com/soo/bibsys/services/BibService.java
+++ src/main/java/br/com/soo/bibsys/services/BibService.java
@@ -1,5 +1,6 @@
 package br.com.soo.bibsys.services;
 
+import br.com.soo.bibsys.utils.FileOperator;
 import br.com.soo.bibsys.utils.Formatter;
 import com.sun.jersey.core.header.ContentDisposition;
 import com.sun.jersey.core.header.FormDataContentDisposition;
@@ -23,11 +24,33 @@ public class BibService {
 	@Path("/format")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response format(@DefaultValue("true") @FormDataParam("enabled") boolean enabled,
-			@FormDataParam("file") InputStream uploadedInputStream,
+	public Response format(@FormDataParam("file") InputStream uploadedInputStream,
 			@FormDataParam("file") FormDataContentDisposition fileDetail) throws IOException {
 
 		final String file = new Formatter().formatFile(IOUtils.toString(uploadedInputStream, Charset.defaultCharset()));
+
+		ContentDisposition contentDisposition = ContentDisposition.type("attachment").fileName("file.bib").creationDate(new Date()).build();
+
+		return Response.ok(new StreamingOutput() {
+			public void write(OutputStream outputStream) throws IOException, WebApplicationException {
+				outputStream.write(file.getBytes());
+			}
+		}).header("Content-Disposition", contentDisposition).build();
+	}
+
+	@POST
+	@Path("/join")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response join(@FormDataParam("file1") InputStream uploadedInputStream1,
+			@FormDataParam("file1") FormDataContentDisposition fileDetail1,
+			@FormDataParam("file2") InputStream uploadedInputStream2,
+			@FormDataParam("file2") FormDataContentDisposition fileDetail2) throws IOException {
+
+		String file1 = new Formatter().formatFile(IOUtils.toString(uploadedInputStream1, Charset.defaultCharset()));
+		String file2 = new Formatter().formatFile(IOUtils.toString(uploadedInputStream2, Charset.defaultCharset()));
+
+		final String file = new FileOperator().joinFiles(file1, file2);
 
 		ContentDisposition contentDisposition = ContentDisposition.type("attachment").fileName("file.bib").creationDate(new Date()).build();
 
